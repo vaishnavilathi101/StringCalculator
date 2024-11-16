6e8d60de7edef2974ffe0b8ac86beac165f07695
vaishnavilathi101 <68653542+vaishnavilathi101@users.noreply.github.com>
2024-11-16 17:45:28 +0530

    Added a usecase where string contains delimiter and numbers to add

--- StringCalculator.js
+++ StringCalculator.js
@@ -2,9 +2,24 @@ function add(inputString) {
   if (typeof inputString !== "string")
     throw new Error(`Please provide string input`);
   if (!inputString) return 0;
+
+  let delimiter = /,|\n/;
+  if (inputString.startsWith("//")) {
+    const parts = inputString.split("\n", 2);
+    delimiter = new RegExp(parts[0].slice(2));
+
+    inputString = parts[1];
+  }
+  const numbers = inputString.split(delimiter).map(Number);
+
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 // Test cases
+console.log(add("//:\n1:2"));
+console.log(add("1,2"));
+console.log(add("1\n2,3"));
+console.log(add("//;\n1;2"));
 console.log(add(""));
 try {
   console.log(add([]));
