46dcf9d1ca92d397a9627bcd509a6d46b1a1dd3f
vaishnavilathi101 <68653542+vaishnavilathi101@users.noreply.github.com>
2024-11-16 17:44:10 +0530

    Added a use case where input is empty string

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,10 +1,11 @@
 function add(inputString) {
   if (typeof inputString !== "string")
     throw new Error(`Please provide string input`);
+  if (!inputString) return 0;
 }
 
 // Test cases
-
+console.log(add(""));
 try {
   console.log(add([]));
 } catch (e) {
